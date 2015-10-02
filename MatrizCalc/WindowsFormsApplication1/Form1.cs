9edded59d381ef487dce98b6a3aedc7e2f695420
mauricio leonardo <mauriciolfsilva@gmail.com>
2015-10-02 08:08:55 -0300

    Update Matrix Calc
    Now, it does the determinant the multiplication by another matrix, but
    it still without the interface.

--- MatrizCalc/WindowsFormsApplication1/Form1.cs
+++ MatrizCalc/WindowsFormsApplication1/Form1.cs
@@ -12,12 +12,12 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        float[,] M1 = new float[2, 3] { { 2, 3, 4}, { 6, 8, 1} };
-        float[,] M2 = new float[2,2] {{1,3},{2,1}};
+        float[,] M1 = new float[2, 2] { { 2, 3}, { 6, 8} };
+        float[,] M2 = new float[4, 4] { { 0,0,1,0.5f }, { 1,2,1,-2 }, {0,-1,3,0} ,{0,-2,4,-4}};
         public Form1()
         {
             InitializeComponent();
-            Sim(M1);
+            Determinante(M2);
         }
 
         public float[,] Add2dArray(float[,] A, float[,] B)
@@ -49,26 +49,31 @@ namespace WindowsFormsApplication1
         }
 
        
-        /*public float[,] Mult2dArray(float[,] A, float[,] B)
+        public float[,] Mult2dArray(float[,] A, float[,] B)
         {
-            float[,] R;
+            float[,] R = new float[A.GetLength(0),B.GetLength(1)];
+
             if (A.GetLength(1) == B.GetLength(0))
             {
-                R = new float[A.GetLength(0), A.GetLength(1)];
                 for (int i = 0; i < A.GetLength(0); i++)
                 {
-                    for (int j = 0; j < A.GetLength(1); j++)
+                    for (int j = 0; j < B.GetLength(1); j++)
                     {
-                        R[i, j] = A[i, j] - B[i, j];
+                        for (int n = 0; n < A.GetLength(1); n++)
+                        {
+                            R[i,j] += A[i,n]*B[n,j];
+                        }
                     }
                 }
             }
             else
             {
-                R = new float[0, 0];
-                return R;
+                MessageBox.Show("Lines of Matrix2 must be equals of columns of Matrix1");
             }
-        }*/
+            
+            
+            return R;
+        }
 
         public float[,] Mult2dArraybyRealNumb(float[,] A, float B)
         {
@@ -122,18 +127,78 @@ namespace WindowsFormsApplication1
                 return false;
         }
 
-       /* public float[,] ArrayGem(int L, int C)
+        public float Determinante(float[,] A)
         {
-            float[,] R = new float[L, C];
-            for (int i = 0; i < L; i++)
-            {
-                for (int j = 0; j < C; j++)
-                {
-                    
-                }
-            }
+            float r = 0;
+            float m = 1;
+            float m2 = 1;
 
-            return R;
-        }*/
+            if (A.GetLength(1) == A.GetLength(0))
+            {
+                if(A.GetLength(0) == 1)
+                {
+                    r = A[0, 0];
+                }
+
+                else if(A.GetLength(0) == 2)
+                {
+                    r = A[0, 0] * A[1, 1] - A[0, 1] * A[1, 0];
+                }
+
+                else if (A.GetLength(0) == 3)
+                {
+                    r = A[0, 0] * A[1, 1] * A[2, 2] + A[0, 1] * A[1, 2] * A[2, 0] + A[0, 2] * A[1, 0] * A[2, 1]
+                      - A[0, 2] * A[1, 1] * A[2, 0] - A[0, 0] * A[1, 2] * A[2, 1] - A[0, 1] * A[1, 0] * A[2, 2];
+                }
+
+                else
+                {
+                    bool isZero = true;
+                    float[] L = new float[A.GetLength(1)];
+
+                    for (int i = 0; i < A.GetLength(0); i++ )
+                    {
+                        if(A[i,0] > 0)
+                        {
+                            isZero = false;
+                            if (i > 0)
+                            {
+                                for (int j = 0; j < A.GetLength(1); j++)
+                                {
+                                    L[j] = A[i, j];
+                                    A[i, j] = A[0, j];
+                                    A[0, j] = L[j];
+                                }
+                                m2 = -1;
+                            }
+                            i = A.GetLength(0);
+                        }                   
+                    }
+
+                    if (!isZero)
+                    {
+                        m /= A[0, 0];
+
+                        float[,] B = new float[A.GetLength(1) - 1, A.GetLength(0) - 1];
+                        for (int i = 0; i < A.GetLength(0); i++)
+                        {
+                            A[i, 0] *= m;
+                        }
+
+                        for (int i = 1; i < A.GetLength(0); i++)
+                        {
+                            for (int j = 1; j < A.GetLength(1); j++)
+                            {
+                                B[i - 1, j - 1] = A[i, j] - (A[0, j] * A[i, 0]);
+                            }
+                        }
+
+                        r = Determinante(B);
+                    }
+                }
+
+            }
+            return r / m * m2;
+        }
     }
 }
